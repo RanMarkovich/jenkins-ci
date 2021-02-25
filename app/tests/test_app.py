8218f9f89e7a9b4be819d10db1107ad3ec30aed2
Ran <ran@minutemedia.com>
2021-02-26 00:39:18 +0200

    fix test

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -4,6 +4,6 @@ import requests
 
 
 def test_hello_flask():
-    r = requests.get('http://flask_app:5000')
+    r = requests.get('http://localhost:5000')
     assert r.status_code == 200, f"expected status code 200, instead got: {r.text}"
     assert r.text == 'hello_flask', f"expected 'hello_flask' in response text, instead got: {r.text}"
