1fa3f62cc4a7173443f57b572886006468b94cdf
Ran <ran@minutemedia.com>
2021-02-28 10:19:58 +0200

    jenkinsfile

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -4,6 +4,6 @@ import requests
 
 
 def test_hello_flask():
-    r = requests.get('http://0.0.0.0:5000')
+    r = requests.get('http://0.0.0.0:8080')
     assert r.status_code == 200, f"expected status code 200, instead got: {r.text}"
     assert r.text == 'hello_flask', f"expected 'hello_flask' in response text, instead got: {r.text}"
